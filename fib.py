b4e835f3ef2c10ae8e72fdc3d017cbc314d09ab5
Nambala raghava <66437443+nambalaraghava-123@users.noreply.github.com>
2020-06-08 12:11:35 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -4,10 +4,18 @@ Fibonnacci series
 n=int(input())
 a=0
 b=1
-print(a,end=" ")
-print(b,end=" ")
-for i in range(n):
-    c=a+b
-    print(c,end=" ")
-    a=b
-    b=c
+if(n==0):
+    print(a,end=" ")
+elif(n==1):
+    print(a,end=" ")
+    print(b,end=" ")
+elif(n<0):
+    print("Enter a positive integer")
+else:
+    print(a,end=" ")
+    print(b,end=" ")
+    for i in range(n-1):
+        c=a+b
+        print(c,end=" ")
+        a=b
+        b=c
